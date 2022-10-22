40403d1e7b2951e0851681a18236fa9f41f2a4a5
sherwyn_b <sherwyn.braganza@mines.sdsmt.edu>
2022-10-22 12:21:08 -0600

    added some comments

--- project2/feature_detection.py
+++ project2/feature_detection.py
@@ -1,28 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import skimage.io
+
 import image_handler
 
 class FeatureDetection:
     def __init__(self, image:np.ndarray = None):
         self.image = image
+        self.features = []
 
     def showFeatures(self, loc: tuple[int, int], scale: float = 1, orientation: float = 0) -> None:
+        """
+            Shows (overlays) the features detected on the image. Plots them in the form of boxes with
+            orientation.
+
+            :param loc: the centroid of where the feature was detected
+            :param scale:
+            :param orientation: The orientation of the feature.
+            :return: None
+        """
         orientation += 90
         buffer = self.constructBufferSquare(loc, scale)
         rotated_buffer = self.rotateBufferSquare(loc, buffer, orientation)
         plotpoints = self.generatePlotPoints(loc, rotated_buffer, scale)
 
         for x in plotpoints:
-            self.image[x[0], x[1], 0] = 255
-            self.image[x[0], x[1], 1] = 0
-            self.image[x[0], x[1], 2] = 0
+            self.image[x[0], x[1], :] = np.asarray([255, 0, 0])
 
     def constructBufferSquare(self, loc: tuple[int,int], scale: float) -> np.ndarray:
         """
             Gets 4 points around loc that corresponds to the edges of the
             buffer square. The points are listed in the bottomLeft, bottomRight,
-            topRight, topLeft, bottomTopRightCenter
-        :param loc:
+            topRight, topLeft, bottomTopRightCenter order
+        :param loc: The centroid of the buffer square
         :param scale:
         :return:
         """
@@ -36,6 +46,13 @@ class FeatureDetection:
         return np.vstack((bottomLeft,bottomRight,topRight,topLeft, bottomTopRightCenter))
 
     def rotateBufferSquare(self, loc: tuple[int, int], points: np.ndarray, orientation: float) -> np.ndarray:
+        """
+            Rotate the buffer square according to the orientation given.
+            :param loc:
+            :param points: The extremities of the buffer square
+            :param orientation:
+            :return:
+        """
         loc = np.asarray(loc)
         cob_points = points - loc
         theta = np.radians(orientation)
@@ -48,6 +65,15 @@ class FeatureDetection:
         return np.rint(cob_points + loc).astype(int, casting='unsafe')
 
     def generatePlotPoints(self, loc: tuple[int, int], points: np.ndarray, scale) -> np.ndarray:
+        """
+            Interpolate between extremities and get points along the edges of the buffer
+            square for plotting
+
+            :param loc:
+            :param points:
+            :param scale:
+            :return:
+        """
         plotpoints = np.asarray([0, 0])
 
         for i in range(len(points)):
@@ -58,8 +84,8 @@ class FeatureDetection:
         plotpoints = np.vstack((
             plotpoints,
             np.hstack((
-                np.linspace(loc[0], points[-1,0], num=scale, endpoint=True, dtype=int).reshape(-1, 1),
-                np.linspace(loc[1], points[-1,1], num=scale, endpoint=True, dtype=int).reshape(-1, 1),
+                np.linspace(loc[0], points[-1, 0], num=scale, endpoint=True, dtype=int).reshape(-1, 1),
+                np.linspace(loc[1], points[-1, 1], num=scale, endpoint=True, dtype=int).reshape(-1, 1),
             ))
         ))
 
@@ -83,6 +109,6 @@ if __name__ == '__main__':
     image = image_handler.readImage('datasets/Yosemite/Yosemite1.jpg')
     features = FeatureDetection(image)
     features.showFeatures((88, 96), 20, 45)
-    features.showFeatures((182, 44), 20, -36)
-    plt.imshow(image)
-    plt.show()
+    features.showFeatures((250, 450), 20, -36)
+    skimage.io.imshow(image)
+    skimage.io.show()
