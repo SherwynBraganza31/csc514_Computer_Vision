475171c1fa5738de8cc260259be96a57eeb388bc
SherwynBraganza31 <sherwyn.braganza@mines.sdsmt.edu>
2022-12-07 23:15:32 -0700

    rearranged stuff. image classification left

--- project4/image_classification.py
+++ project4/image_classification.py
@@ -11,8 +11,6 @@ import skimage
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import color
-from typing import List
-import cv2
 from skimage import io
 import matplotlib
 
@@ -20,7 +18,7 @@ import matplotlib
 path_list = [fn for fn in os.listdir("Archive/TrainingImages/TrainingImages/")]
 
 
-def getImageSpace(folder_name: str) -> np.ndarray:
+def getImageSpace(folder_name: str, grab_entire=False) -> np.ndarray:
     """
         Load all the train images from a folder and stack them together horizontally
         to create X matrix or image_space matrix
@@ -29,16 +27,50 @@ def getImageSpace(folder_name: str) -> np.ndarray:
         :return: X or image_space matrix
     """
     image_space = np.zeros((128*128, 0))
-    for i in range(128):
-        img = io.imread(f"Archive/TrainingImages/TrainingImages/{folder_name}/UnProcessed/img_{i}.png")
-        img = img.reshape((-1, 1))
-        image_space = np.hstack((image_space, img))
-        
+    if grab_entire:
+        for file in path_list:
+            for i in range(128):
+                img = skimage.io.imread(f"Archive/TrainingImages/TrainingImages/{file}/UnProcessed/img_{i}.png",
+                                        as_gray=True)
+                img = img.reshape((-1, 1))
+                image_space = np.hstack((image_space, img))
+    else:
+        for i in range(128):
+            img = io.imread(f"Archive/TrainingImages/TrainingImages/{folder_name}/UnProcessed/img_{i}.png")
+            img = img.reshape((-1, 1))
+            image_space = np.hstack((image_space, img))
+
     #print(image_space.shape)
     return image_space
 
 
-def computeER(X: np.ndarray, singular_vals, thresh: float) -> tuple:
+def loadTestImage(folder_name: str, grab_entire=False) -> np.ndarray:
+    """
+        Load all the train images from a folder and stack them together horizontally
+        to create X matrix or image_space matrix
+
+        :param folder_name: name of the parent folder
+        :return: X or image_space matrix
+    """
+    image_space = np.zeros((128*128, 0))
+    if grab_entire:
+        for file in path_list:
+            for i in range(128):
+                img = skimage.io.imread(f"Archive/TestImages/TestImages/{file}/UnProcessed/img_{i}.png",
+                                        as_gray=True)
+                img = img.reshape((-1, 1))
+                image_space = np.hstack((image_space, img))
+    else:
+        for i in range(128):
+            img = io.imread(f"Archive/TestImages/TestImages/{folder_name}/UnProcessed/img_{i}.png")
+            img = img.reshape((-1, 1))
+            image_space = np.hstack((image_space, img))
+
+    #print(image_space.shape)
+    return image_space
+
+
+def computeER(X: np.ndarray, thresh: float) -> tuple:
     """
         Computes the order of eigenvalues required to achieve a
         energy reconstruction specified by thresh.
@@ -51,6 +83,8 @@ def computeER(X: np.ndarray, singular_vals, thresh: float) -> tuple:
     ratio_list = []
     frob_norm = (X * X).sum()
     ratio = 0
+    _, singular_vals, _ = np.linalg.svd(X, full_matrices=False)
+
     for sigma in singular_vals:
         ratio += (sigma**2)/frob_norm
         ratio_list.append(ratio)
@@ -58,33 +92,15 @@ def computeER(X: np.ndarray, singular_vals, thresh: float) -> tuple:
             break
 
     return len(ratio_list), ratio_list
-    
 
-# def createManifold(image_list, typ):
-#     """
-#
-#         :param typ: L for local manifold construction, G for global
-#         :return:
-#     """
-#     manifold = np.zeros((128*128, 0))
-#     for fn in image_list:
-#         X = getImageSpace(fn)
-#         if typ == "L":
-#             U, S, V = np.linalg.svd(X, full_matrices=False)
-#             k, energy_list = ComputeER(X, S, 0.9)
-#             manifold = np.hstack((manifold, U[:, 0:k]))
-#         else:
-#             manifold = np.hstack((manifold, X))
-#
-#     return manifold
 
 def createManifold(image_space):
-    k = 3
-    # k, _ = computeER(image_space, singular_vals, 0.9)
+    k, _ = computeER(image_space, 0.9)
     eigen_vectors = getBasisVectors(image_space, k)
     manifold = np.dot(image_space.T, eigen_vectors)
 
-    return manifold
+    return manifold, eigen_vectors
+
 
 def getUnbiasedDataset(image_space: np.ndarray) -> np.ndarray:
     """
@@ -108,49 +124,57 @@ def getBasisVectors(image_space: np.ndarray, k: int) -> np.ndarray:
     """
     U, S, V = np.linalg.svd(image_space, full_matrices=False)
 
-    return U[k]
+    return U[:, 0:k]
 
-
+# TODO
 def plotEnergyRecovery(imagelist):
     subplot_nums = np.sqrt(len(imagelist))
     fig, ax = plt.subplots(subplot_nums, subplot_nums)
 
 
-if __name__ == '__main__':
-    mf = createManifold(path_list, "L")
-    print(mf.shape)
-    U, S, V = np.linalg.svd(mf, full_matrices=False)
-    for i in range(10):
-        eimg = U[:, i].reshape((128,128))
-        plt.imshow(eimg, cmap="gray")
+def findClass(manifolds, eigen_vectors,  test_image):
+    test_projection = np.dot(test_image, eigen_vectors['global'])
+    min_distance = float('-inf')
+    min_idx = 0
+
+    for idx in range(manifolds['global'].shape[0]):
+        temp_val = np.sum((test_projection - manifolds['global'][idx]) ** 2)
+        if temp_val < min_distance:
+            min_distance = temp_val
+            min_idx = idx
+
+    return path_list[min_idx//128]
+
+
+def plotEigVector(eig_vec, name: str, show=False):
+    eig_vec = (eig_vec - eig_vec.min())/(eig_vec.max() - eig_vec.min())
+    eig_vec = skimage.img_as_uint(eig_vec.reshape(128, 128))
+    if show:
+        plt.title(f'EigenVector of {name}')
+        plt.imshow(eig_vec, cmap='gray')
         plt.show()
-    
-'''
-img_space = getImageSpace("Cup64")
+    skimage.io.imsave(f'eig_{name}.jpg', eig_vec)
 
 
-avg = np.mean(img_space, axis=0)
-#img_space = img_space - avg
-#print(avg)
-#img_space = np.outer(img_space, img_space.T) / 128
-U, S, V = np.linalg.svd(img_space, full_matrices=False)
-print(U.shape)
-#w, v = np.linalg.eig(U)
-#print(V.shape)
+if __name__ == '__main__':
+    manifolds = {}
+    eigen_vectors = {}
+    for obj in path_list[0:8]:
+        mani, eig = createManifold(getImageSpace(obj))
+        manifolds.update({obj: mani})
+        eigen_vectors.update({obj: eig})
 
-sh, li = ComputeER(img_space, S, 0.9)
-img_space = img_space[0:sh[0],0:sh[1]]
-print(img_space.shape)
-#print(li)
+    mani, eig = createManifold(getImageSpace("", grab_entire=True))
+    manifolds.update({'global': mani})
+    eigen_vectors.update({'global': eig})
 
-print(S)
-print( (S*S).sum())
-for i in range(10):
-    eimg = U[:, i].reshape((128,128))
-    plt.imshow(eimg, cmap="gray")
-    plt.show()
-'''
+    # plotEigVector(eigen_vectors['Boat64'][:, 0], 'Boat64')
+    # plotEigVector(eigen_vectors['Keyboard64'][:, 0], 'Keyboard64')
+    # plotEigVector(eigen_vectors['Cup64'][:, 0], 'Cup64')
+    # plotEigVector(eigen_vectors['Cup64'][:, 1], 'Cup64_2')
+    # plotEigVector(eigen_vectors['Cup64'][:, 2], 'Cup64_3')
+    # plotEigVector(eigen_vectors['global'][:, 0], 'global_first')
+    # plotEigVector(eigen_vectors['global'][:, 3], 'global_other')
 
-#print(U.shape)
-#print(V.shape)
-#print(S.shape)
+    obj = findClass(manifolds, eigen_vectors, np.zeros((128*128)))
+    test_images = loadTestImage(path_list[0])
